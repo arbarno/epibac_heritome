b4b14a2446c88ddbb2b0b1818f773fe854b763be
Adam R. Barno <89023073+arbarno@users.noreply.github.com>
2026-08-27 14:26:54 +0200

    Update bash_methylation_analysis.sh

--- bash_methylation_analysis.sh
+++ bash_methylation_analysis.sh
@@ -28,11 +28,17 @@ for PILEUP in *.traditional.pileup.bed; do
     awk '$5 >= 10' "${PILEUP}" > "${SAMPLE_NAME}.10x.pileup.bed"
 done
 
+# Change the second position of the bed file to look for conserved dinucleotides (CG) in the next step
+for PILEUP in *.10x.pileup.bed; do
+    SAMPLE_NAME=$(basename "${PILEUP}" .10x.pileup.bed)
+    awk -F'\t' 'BEGIN{OFS="\t"} {$3=$2+2; print}' "${PILEUP}" > "${SAMPLE_NAME}.10x.cpgs.bed"
+done
+
 # Filter out positions that are not conserved with clair3
 # clair3 v2.0.2 (https://github.com/HKU-BAL/clair3)
 conda activate clair3
-for BED in *.10x.pileup.bed; do
-    SAMPLE_NAME=$(basename "${BED}" .10x.pileup.bed)
+for BED in *.10x.cpgs.bed; do
+    SAMPLE_NAME=$(basename "${BED}" .10x.cpgs.bed)
     run_clair3.sh \
         --bam_fn="${SAMPLE_NAME}.aligned.bam" \
         --ref_fn="${REF_ASSEMBLY}" \
@@ -45,15 +51,33 @@ for BED in *.10x.pileup.bed; do
 done
 conda deactivate
 
-# Sort filtered pileup files
-# bedtools v2.30.0 (https://bedtools.readthedocs.io/en/latest/index.html)
-for BED in *.10x.pileup.bed; do
+# Filter out CpG variants -> keep the CpG positions that don't PASS as variants
+# bedtools/2.30.0 bcftools/1.16
+for BED in *.10x.cpgs.bed; do
+    SAMPLE_NAME=$(basename "${BED}" .10x.cpgs.bed)
+    bcftools view -f PASS "${SAMPLE_NAME}_clair3"/merge_output.vcf.gz clair3_pass/"${SAMPLE_NAME}_pass.vcf"
+    bedtools intersect -a "${BED}" -b clair3_pass/"${SAMPLE_NAME}_pass.vcf" -v 10x_clair3_filt/"${SAMPLE_NAME}.10x.clair3.filt.bed"
+done
+
+# Go back to just the C positions (from CpGs)
+cd 10x_clair3_filt
+for BED in *.10x.clair3.filt.bed; do
+    SAMPLE_NAME=$(basename "${BED}" .10x.clair3.filt.bed)
+    awk -F'\t' 'BEGIN{OFS="\t"} {$3=$2+1; print}' "${BED}" > ${SAMPLE_NAME}.10x.clair3.filt.Cs.bed
+done
+
+# Sort filtered clair3 passing files
+# bedtools v2.30.0
+cd 10x_clair3_filt
+for BED in *.10x.clair3.filt.Cs.bed; do
     sort -k1,1 -k2,2n "${BED}" > "${BED}.sorted"
 done
 
-# Filter so just the CpG positions with 10x coverage in across all samples remain
-bedtools intersect -a "${BED_FILES[0]}" "${BED_FILES[@]:1/#/-b }" -c > common_positions_w_counts.bed
-awk -v num_files=11 '$NF == num_files' common_positions_w_counts.bed > common_positions.bed
+# Filter each sample to have conserved CpG positions across all samples
+SORTED_BEDS=(*.10x.clair3.filt.Cs.bed.sorted)
+NUM_SAMPLES=${#SORTED_BEDS[@]}
+bedtools multiinter -i "${SORTED_BEDS[@]}" > multiinter_output.bed
+awk -v n="${NUM_SAMPLES}" '$4 == n {print $1"\t"$2"\t"$3}' multiinter_output.bed | sort -k1,1 -k2,2n -u > common_positions_clair3.bed
 
 # ---- Use this data to generate the files for NanoMethViz ----
 
@@ -62,7 +86,7 @@ awk -v num_files=11 '$NF == num_files' common_positions_w_counts.bed > common_po
 # and will be used for analyzing methylation using NanoMethViz
 for BAM_FILE in *.bam; do
     SAMPLE_NAME=$(basename "${BAM_FILE}" .bam)
-    modkit extract full --include-bed common_positions.bed --ref "${REF_ASSEMBLY}" "${BAM_FILE}" "${SAMPLE_NAME}.modkit_extract.tsv"
+    modkit extract full --include-bed common_positions_clair3.bed --ref "${REF_ASSEMBLY}" "${BAM_FILE}" "${SAMPLE_NAME}.modkit_extract.tsv"
 done
 gzip *.modkit_extract.tsv
 
@@ -74,6 +98,9 @@ for FILE in *.modkit_extract.tsv.gz; do
         NR > 1 {print sample, $0}' > ${FILE%.tsv.gz}_samp.tsv
 done
 
+# Extract the header from the first file and save it to the combined file
+head -n 1 $(ls *_samp.tsv | head -n 1) > combined_data_tmp.tsv
+
 # Combine all sample files (without headers)
 for FILE in *_samp.tsv; do
     tail -n +2 "$FILE" >> combined_data_tmp.tsv
@@ -81,36 +108,40 @@ done
 
 # Sort and create tabix file for NanoMethViz
 # samtools v1.16.1 (https://github.com/samtools/samtools)
-awk -F'\t' -v OFS='\t' '{ print $1, $5, $4, $6, $14, $2 }' combined_data_tmp.tsv > combined_data.tsv
-sort -k2,2 -k3,3n combined_data.tsv > combined_data.sorted.tsv
-bgzip combined_data.sorted.tsv
-tabix -s 2 -b 3 -e 3 combined_data.sorted.tsv.gz
+awk -F'\t' -v OFS='\t' '{ print $1, $5, $4, $6, $14, $2 }' combined_data_tmp.tsv > combined_data_clair3.tsv
+sort -k2,2 -k3,3n combined_data_clair3.tsv > combined_data_clair3.sorted.tsv
+bgzip combined_data_clair3.sorted.tsv
+tabix -s 2 -b 3 -e 3 combined_data_clair3.sorted.tsv.gz
 
 # ---- Return to methylation pipeline ----
 
 # Filter individual pileup files by common positions
-for BED in *.10x.pileup.bed.sorted; do
+cd 10x_clair3_filt
+for BED in *.clair3.filt.Cs.bed.sorted; do
     SAMPLE_NAME=$(basename "${BED}" .10x.pileup.bed.sorted)
-    bedtools intersect -a ${BED} -b common_positions.bed > ${SAMPLE_NAME}.filtered.pileup.bed
+    bedtools intersect -a ${BED} -b common_positions_clair3.bed > ${SAMPLE_NAME}.clair3.filt.conservedCpGs.bed
 done
 
 # ---- The following steps use python scripts found here: https://github.com/arbarno/epibac_heritome/tree/main/python_scripts ----
 
 # Merge filtered pileup files to create a single file with compiled methylation data
 # python v3.11.0
-ALL_FILTERED=$(echo *.filtered.pileup.bed)
-merge_bed_files.py ${ALL_FILTERED} -v > merged_bed_output.bed
+cd 10x_clair3_filt
+ALL_FILTERED=$(echo *.clair3.filt.conservedCpGs.bed)
+merge_bed_files.py ${ALL_FILTERED} -v > merged_clair3_bed_output.bed
 
 # Annotate merged BED file
 # python v3.11.0
-annotate_merged_bed.py ${REF_ASSEMBLY} ${REF_GFF} merged_bed_output.bed > merged_bed_annotated.bed
-cut -f7 merged_bed_annotated.bed | sort -u > gene_universe.txt
+cd 10x_clair3_filt
+annotate_merged_bed.py ${REF_ASSEMBLY} ${REF_GFF} merged_clair3_bed_output.bed > merged_clair3_bed_annotated.bed
+cut -f7 merged_clair3_bed_annotated.bed | sort -u > clair3_gene_universe.txt
 
 # Add annotation to filtered pileup files
-cut -f 7- merged_bed_annotated.bed > tmp
-for BED in *filtered.pileup.bed; do
-    SAMPLE_NAME=$(basename "${BED}" .filtered.pileup.bed)
-    cut -f1,2,3,11,12,10 ${BED} | paste - tmp > ${SAMPLE_NAME}_filtered_annotated.bed
+cd 10x_clair3_filt
+cut -f 7- merged_clair3_bed_annotated.bed > tmp
+for BED in *.clair3.filt.conservedCpGs.bed; do
+    SAMPLE_NAME=$(basename "${BED}" .clair3.filt.conservedCpGs.bed)
+    cut -f1,2,3,11,12,10 ${BED} | paste - tmp > ${SAMPLE_NAME}.clair3.filt.conservedCpGs.annotated.bed
 done
 
 # Calculate CpG bias
@@ -119,20 +150,20 @@ calc_cpg_bias.py ${REF_ASSEMBLY} ${REF_GFF} > /ibex/project/c2208/nanopore/outpu
 
 # Calculate mean methylation per sample
 # python v3.11.0
-for BED in *_filtered_annotated.bed; do
-    SAMPLE_NAME=$(basename "${BED}" _filtered_annotated.bed)
-    calc_mean_methylation.py ${REF_ASSEMBLY} ${REF_GFF} ${BED} > ${SAMPLE_NAME}_mean_meths.tsv
+for BED in *.clair3.filt.conservedCpGs.annotated.bed; do
+    SAMPLE_NAME=$(basename "${BED}" .clair3.filt.conservedCpGs.annotated.bed)
+    calc_mean_methylation.py ${REF_ASSEMBLY} ${REF_GFF} ${BED} > ${SAMPLE_NAME}_clair3_mean_meths.tsv
 done
 
 # Tabulate all mean methylation files
 # python v3.11.0
-ALL_FILTERED=$(echo *filtered_annotated.bed)
-tabulate_tsvs.py ${ALL_FILTERED} -k 0 1 -c 3 -v > all_mean_meths.tsv
+cd 10x_clair3_filt
+ALL_FILTERED=$(echo *_mean_meths.tsv)
+tabulate_tsvs.py ${ALL_FILTERED} -k 0 1 -c 3 -v > all_clair3_mean_meths.tsv
 
 # Tabulate filtered context percentages
 # python v3.11.0
-ALL_FILTERED=$(echo *filtered_annotated.bed)
-tabulate_tsvs.py ${ALL_FILTERED} -k 0 1 6 10 -c 5 -v > all_filt_pct_context.tsv
-sed -i '' 's/^\t\t/scaffold\tpos\tgene\tcontext/' all_filt_pct_context.tsv
-sed -i '' 's/\.bed//g' all_filt_pct_context.tsv
+ALL_FILTERED=$(echo *.clair3.filt.conservedCpGs.annotated.bed)
+tabulate_tsvs.py ${ALL_FILTERED} -k 0 1 6 10 -c 4 -v > all_clair3_pct_context.tsv
+sed -i 's/^\t\t\t/scaffold\tpos\tgene\tcontext/' all_clair3_pct_context.tsv && sed -i 's/\.bed//g' all_clair3_pct_context.tsv
 gzip all_filt_pct_context.tsv
